42cb456b441be9b17f419cdf292d96de6ec8d59a
3vilbird <devilisnotinhome@gmail.com>
2022-04-30 00:13:27 +0530

    completed assignment

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./index.css";
+import App from "./App";
+// import Routes from "./routes/Routes";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./components/Login/Login";
+
+ReactDOM.render(
+  <App />,
+  document.getElementById("root")
+);
